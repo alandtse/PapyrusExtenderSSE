5418064141ea9b52cbf10e203c21cf44e9bf29e6
Alan Tse <alandtse@gmail.com>
2025-07-28 02:41:24 -0700

    fix(VR): fix allocation errors

--- src/main.cpp
+++ src/main.cpp
@@ -102,7 +102,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 
 	SKSE::Init(a_skse, false);
 
-	SKSE::AllocTrampoline(208);
+	SKSE::AllocTrampoline(OFFSET_3(208, 208, 236));
 
 	const auto papyrus = SKSE::GetPapyrusInterface();
 	papyrus->Register(Papyrus::Bind);
